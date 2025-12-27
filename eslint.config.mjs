97b9ccb7291d3de931cbd2c733d4e7fbe026d563
E <e@elzr.me>
2025-12-27 09:56:07 -0500

    fix eslint + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config"
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
-import prettier from "eslint-config-prettier/flat"
+import prettierPlugin from "eslint-plugin-prettier/recommended"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  prettier,
+  prettierPlugin,
   {
     rules: {
+      "import/order": "warn",
       "no-console": ["warn", { allow: ["error", "warn"] }],
       "@typescript-eslint/no-unused-vars": [
         "warn",
@@ -18,16 +19,10 @@ const eslintConfig = defineConfig([
           caughtErrorsIgnorePattern: "^_",
         },
       ],
+      "prettier/prettier": "warn",
     },
   },
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-    "node_modules",
-    "dev/*",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", "node_modules", "dev/*"]),
 ])
 
 export default eslintConfig
